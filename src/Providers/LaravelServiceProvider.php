4fc17c36955574dc8dd1c294342232823d23201a
Jiannei <longjian.huang@foxmail.com>
2021-09-21 19:02:45 +0800

    fix: samples

--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -20,14 +20,10 @@ class LaravelServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([$adminPath => config_path('pear/admin.php')], 'pear');
-            $this->publishes([$menuPath => config_path('pear/admin.php')], 'pear');
+            $this->publishes([$menuPath => config_path('pear/menu.php')], 'pear');
             $this->publishes([dirname(__DIR__, 2).'/resources/assets' => public_path('pear')], 'assets');
 
-            $this->publishes([dirname(__DIR__, 2).'/samples/config' => public_path('admin-sample/config')], 'samples');
-            $this->publishes([dirname(__DIR__, 2).'/samples/css' => public_path('admin-sample/css')], 'samples');
-            $this->publishes([dirname(__DIR__, 2).'/samples/data' => public_path('admin-sample/data')], 'samples');
-            $this->publishes([dirname(__DIR__, 2).'/samples/images' => public_path('admin-sample/images')], 'samples');
-            $this->publishes([dirname(__DIR__, 2).'/samples/views' => resource_path('views/admin-sample')], 'samples');
+            $this->publishes([dirname(__DIR__, 2).'/samples' => public_path('admin-sample')], 'samples');
         }
 
         $this->mergeConfigFrom($adminPath, 'pear');
