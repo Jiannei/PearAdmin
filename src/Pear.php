239a465b5e94ccb199e11aa70fa91eb2cc7ca41d
Jiannei <longjian.huang@foxmail.com>
2021-09-25 09:55:42 +0800

    revert: 移除辅助函数

--- src/Pear.php
+++ src/Pear.php
@@ -2,13 +2,8 @@
 
 namespace Jiannei\PearAdmin;
 
-use Illuminate\Support\Facades\URL;
-
 class Pear
 {
-    protected $configPrefix = 'pear.';
-    protected $assetPrefix = 'pear/';
-
     /**
      * Pear Admin version
      *
@@ -17,31 +12,5 @@ class Pear
     public function version()
     {
         return 'Pear Admin v3.9.0';
-    }
-
-    /**
-     * Get / set the specified configuration value.
-     *
-     * If an array is passed as the key, we will assume you want to set an array of values.
-     *
-     * @param  array|string|null  $key
-     * @param  mixed  $default
-     * @return mixed|\Illuminate\Config\Repository
-     */
-    public function config($key = null, $default = null)
-    {
-        return config($this->configPrefix.$key, $default);
-    }
-
-    /**
-     * Generate Pear Admin asset path for the application.
-     *
-     * @param  string  $path
-     * @param  bool|null  $secure
-     * @return string
-     */
-    public function asset(string $path, bool $secure = null)
-    {
-        return URL::asset($this->assetPrefix.$path, $secure);
     }
 }
--- src/Support/Facades/Pear.php
+++ src/Support/Facades/Pear.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Facade as LaravelFacade;
 
 /**
  * @method static version()
- * @method static config($key = null, $default = null)
  * @see \Jiannei\PearAdmin\Pear
  */
 class Pear extends LaravelFacade
