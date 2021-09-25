c7e44f8177afaf3d52d50cc2ea1ce71e7a6e690a
Jiannei <longjian.huang@foxmail.com>
2021-09-25 09:51:20 +0800

    revert: 移除配置文件

--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -13,20 +13,10 @@ class LaravelServiceProvider extends ServiceProvider
 
     protected function setupConfig()
     {
-        $basePath = dirname(__DIR__, 2).'/config/';
-
-        $adminPath = $basePath.'admin.php';
-        $menuPath = $basePath.'menu.php';
-
         if ($this->app->runningInConsole()) {
-            $this->publishes([$adminPath => config_path('pear/admin.php')], 'pear');
-            $this->publishes([$menuPath => config_path('pear/menu.php')], 'pear');
             $this->publishes([dirname(__DIR__, 2).'/resources/assets' => public_path('pear')], 'assets');
 
             $this->publishes([dirname(__DIR__, 2).'/samples' => public_path('admin')], 'samples');
         }
-
-        $this->mergeConfigFrom($adminPath, 'pear.admin');
-        $this->mergeConfigFrom($menuPath, 'pear.menu');
     }
 }
