0a3687a7c2783c9e644a28f4879e418d951d5b54
Jiannei <Jiannei@users.noreply.github.com>
2021-10-31 13:45:36 +0000

    Apply fixes from StyleCI

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,17 @@
 <?php
 
-if (!function_exists('lay_admin_version')) {
+/*
+ * This file is part of the jiannei/layadmin.
+ *
+ * (c) jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+if (! function_exists('lay_admin_version')) {
     /**
-     * Get Lay Admin version
+     * Get Lay Admin version.
      *
      * @return string
      */
