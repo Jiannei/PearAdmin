120499d69d5d3f23e7657ea6e845e2f4d82a34ef
Jiannei <Jiannei@users.noreply.github.com>
2021-11-04 16:21:23 +0000

    Apply fixes from StyleCI

--- src/LayAdmin.php
+++ src/LayAdmin.php
@@ -55,14 +55,14 @@ class LayAdmin
         $paths = explode('/', optional(request())->path());
 
         if (current($paths) !== config('layadmin.path_prefix')) {
-            throw new InvalidPageConfigException("Route path prefix error.");
+            throw new InvalidPageConfigException('Route path prefix error.');
         }
 
         array_splice($paths, 1, 0, 'config');
 
         $pageConfigPath = base_path('public/').implode(DIRECTORY_SEPARATOR, $paths).'.json';
 
-        if (!file_exists($pageConfigPath)) {
+        if (! file_exists($pageConfigPath)) {
             throw new InvalidPageConfigException("View config json file [$pageConfigPath] not exist.");
         }
 
