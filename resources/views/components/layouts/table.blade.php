a3731dd3d57cf7052c392607b6f279ab5cf2a6ef
Jiannei <longjian.huang@foxmail.com>
2021-11-05 18:26:33 +0800

    pref: table layout

--- resources/views/components/layouts/table.blade.php
+++ resources/views/components/layouts/table.blade.php
@@ -7,17 +7,47 @@
                     <div class="layui-form-item">
                         @foreach($search['items'] as $k => $formItem)
                             {{-- todo 换行限制--}}
-                            <div class="layui-form-item layui-inline">
-                                <label class="layui-form-label" for="{{ $formItem['id'] }}">{{ $formItem['label'] }}</label>
-                                <div class="layui-input-inline">
-                                    @switch($formItem['type'])
-                                        @case('text')
-                                        <input type="{{ $formItem['type'] }}" id="{{ $formItem['id'] }}" name="{{ $formItem['name'] }}" placeholder="{{ $formItem['placeholder'] }}"
-                                               class="layui-input">
-                                        @break;
-                                    @endswitch
+                            @if($formItem['element'] === 'laydate')
+                                <div class="layui-form-item layui-inline">
+                                    <label class="layui-form-label" for="{{ $formItem['id'] }}">{{ $formItem['label'] }}</label>
+                                    @if($range = $formItem['range'] ?? [])
+                                        <div class="layui-inline" id="{{ $formItem['id'] }}">
+                                            <div class="layui-input-inline">
+                                                <input type="text" id="{{ str_replace('#','',$range[0]) }}" name="{{ $formItem['name'] }}[]" class="layui-input"
+                                                       placeholder="{{ $formItem['placeholder'][0] ?? '开始时间'}}"
+                                                       autocomplete="off">
+                                            </div>
+                                            <div class="layui-form-mid">-</div>
+                                            <div class="layui-input-inline">
+                                                <input type="text" id="{{ str_replace('#','',$range[1]) }}" name="{{ $formItem['name'] }}[]" class="layui-input"
+                                                       placeholder="{{ $formItem['placeholder'][1] ?? '结束时间'}}"
+                                                       autocomplete="off">
+                                            </div>
+                                        </div>
+                                    @endif
                                 </div>
-                            </div>
+                            @else
+                                <div class="layui-form-item layui-inline">
+                                    <label class="layui-form-label" for="{{ $formItem['id'] }}">{{ $formItem['label'] }}</label>
+                                    <div class="layui-input-inline">
+                                        @if($formItem['element'] === 'input')
+                                            @switch($formItem['type'])
+                                                @case('text')
+                                                <input type="{{ $formItem['type'] }}" id="{{ $formItem['id'] }}" name="{{ $formItem['name'] }}"
+                                                       placeholder="{{ $formItem['placeholder'] }}"
+                                                       class="layui-input">
+                                                @break;
+                                            @endswitch
+                                        @endif
+
+                                        @if($formItem['element'] === 'select')
+                                            @if($formItem['type'] === 'radio')
+                                                <select name="{{ $formItem['name'] }}" id="{{ $formItem['id'] }}"></select>
+                                            @endif
+                                        @endif
+                                    </div>
+                                </div>
+                            @endif
                         @endforeach
 
                         <div class="layui-form-item layui-inline">
