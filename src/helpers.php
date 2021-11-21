80543e881eeec8e385d0b66d6192a05d9d57f4df
Jiannei <longjian.huang@foxmail.com>
2021-11-21 13:18:58 +0800

    feat: lumen asset 函数

--- src/helpers.php
+++ src/helpers.php
@@ -8,3 +8,17 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
+if (! function_exists('asset')) {
+    /**
+     * Generate an asset path for the application.
+     *
+     * @param  string  $path
+     * @param  bool|null  $secure
+     * @return string
+     */
+    function asset($path, $secure = null)
+    {
+        return app('url')->asset($path, $secure);
+    }
+}
