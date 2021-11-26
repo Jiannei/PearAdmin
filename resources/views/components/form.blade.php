409fb184cf5b3bf38d92c9dfaf416ac3d5b37e8d
Jiannei <longjian.huang@foxmail.com>
2021-11-26 15:10:26 +0800

    feat: form 元素支持隐藏

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,25 +1,25 @@
 <form action="javascript:void(0);" @foreach($form['attributes'] as $key => $val) {{ $key }}="{{ $val }}" @endforeach>
-    {{--  表单内容  --}}
-    <div class="mainBox">
-        <div class="main-container">
-            @foreach($form['items'] as $item)
-                <div class="layui-form-item">
-                    <label class="layui-form-label" for="{{ $item['attributes']['id'] }}">{{ $item['label']}}</label>
-                    <div class="layui-input-block">
-                        <{{$item['element']}} @foreach($item['attributes'] as $key => $val) {{ $key }}="{{ $val }}" @endforeach></{{$item['element']}}>
-                    </div>
+{{--  表单内容  --}}
+<div class="mainBox">
+    <div class="main-container">
+        @foreach($form['items'] as $item)
+            <div class="layui-form-item @if($item['hidden'] ?? '') layui-hide @endif" data-id="{{$item['attributes']['id']}}">
+                <label class="layui-form-label" for="{{ $item['attributes']['id'] }}">{{ $item['label']}}</label>
+                <div class="layui-input-block">
+                    <{{$item['element']}} @foreach($item['attributes'] as $key => $val) {{ $key }}="{{ $val }}" @endforeach></{{$item['element']}}>
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
     </div>
-    {{-- 表单底部操作按钮  --}}
-    <div class="bottom">
-        <div class="button-container">
-            @foreach($form['actions'] as $action)
-                <{{$action['element']}} @foreach($action['attributes'] as $key => $val) {{ $key }}="{{ $val }}" @endforeach>
+</div>
+{{-- 表单底部操作按钮  --}}
+<div class="bottom">
+    <div class="button-container">
+        @foreach($form['actions'] as $action)
+            <{{$action['element']}} @foreach($action['attributes'] as $key => $val) {{ $key }}="{{ $val }}" @endforeach>
                 {!! $action['content'] !!}
-                </{{$action['element']}}>
-            @endforeach
-        </div>
+            </{{$action['element']}}>
+        @endforeach
     </div>
+</div>
 </form>
