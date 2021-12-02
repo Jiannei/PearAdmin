ea3cd810fb6c8132b1a25d9822933e64d84d57a4
Jiannei <longjian.huang@foxmail.com>
2021-12-02 11:25:37 +0800

    pref: 统一 action 定义

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,7 +1,7 @@
 {{--  数据表格：表头操作 --}}
-@if(count($table['toolbar']))
+@if(count($table['toolbar']['actions']))
 <script type="text/html" id="{{ $table['attributes']['id'].'-toolbar' }}">
-    <x-layadmin::action :actions="$table['toolbar']"></x-layadmin::action>
+    <x-layadmin::action :actions="$table['toolbar']['actions']"></x-layadmin::action>
 </script>
 @endif
 
