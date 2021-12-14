d7b726474cde6abc0f94a572ff0e1701fb90a011
Jiannei <longjian.huang@foxmail.com>
2021-12-14 09:33:03 +0800

    fix: conf

--- samples/js/login.js
+++ samples/js/login.js
@@ -1,4 +1,4 @@
-layui.use(['form', 'button', 'popup', 'http', 'sliderVerify'], function () {
+layui.use(['form', 'button', 'popup', 'http'], function () {
   if (window !== top) {
     top.location.href = location.href;
   }
@@ -6,30 +6,21 @@ layui.use(['form', 'button', 'popup', 'http', 'sliderVerify'], function () {
   var form = layui.form;
   var button = layui.button;
   var popup = layui.popup;
-  var sliderVerify = layui.sliderVerify;
-
-  var slider = sliderVerify.render({
-    elem: '#slider',
-  })
 
   // 登 录 提 交
   form.on('submit(login)', function (data) {
     /// 验证
     /// 登录
     /// 动画
-    if (slider.isOk()) {
-      button.load({
-        elem: '.login',
-        time: 1000,
-        done: function () {
-          popup.success("登录成功", function() {
-            location.href = "code.html"
-          });
-        }
-      });
-    }else{
-      layer.msg("请先通过滑块验证");
-    }
+    button.load({
+      elem: '.login',
+      time: 1000,
+      done: function () {
+        popup.success("登录成功", function () {
+          location.href = "code.html"
+        });
+      }
+    });
 
     return false;
   });
