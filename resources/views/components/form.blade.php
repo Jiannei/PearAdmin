fe14fea4d35ddba6a4394dd1c21c113fdb1c2996
Jiannei <longjian.huang@foxmail.com>
2021-12-17 17:30:49 +0800

    feat: 支持渲染 radio

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,22 +1,33 @@
 <form action="javascript:void(0);" @foreach($form['attributes'] as $key => $val) {{ $key }}="{{ $val }}" @endforeach>
-{{--  表单内容  --}}
-<div class="mainBox">
-    <div class="main-container">
-        {{ $slot }}
-        @foreach($form['items'] as $item)
-            <div class="layui-form-item @if($item['hidden'] ?? '') layui-hide @endif" data-id="{{$item['attributes']['id']}}">
-                <label class="layui-form-label" for="{{ $item['attributes']['id'] }}">{{ $item['label']}}</label>
-                <div class="layui-input-block">
-                    <{{$item['element']}} @foreach($item['attributes'] as $key => $val) {{ $key }}="{{ $val }}" @endforeach></{{$item['element']}}>
+    {{--  表单内容  --}}
+    <div class="mainBox">
+        <div class="main-container">
+            {{ $slot }}
+            @foreach($form['items'] as $item)
+                @php
+                    $single = \Illuminate\Support\Arr::isAssoc($item['attributes']);
+                    $dataId = $single ? $item['attributes']['id'] : head($item['attributes'])['name'];
+                @endphp
+
+                <div class="layui-form-item @if($item['hidden'] ?? '') layui-hide @endif" data-id="{{ $dataId }}">
+                    <label class="layui-form-label" for="{{ $dataId }}">{{ $item['label']}}</label>
+                    <div class="layui-input-block">
+                        @if($single)
+                            <{{$item['element']}} @foreach($item['attributes'] as $key => $val) {{ $key }}="{{ $val }}" @endforeach></{{$item['element']}}>
+                        @else
+                            @foreach($item['attributes'] as $value)
+                                <{{$item['element']}} @foreach($value as $key => $val) {{ $key }}="{{ $val }}" @endforeach></{{$item['element']}}>
+                            @endforeach
+                        @endif
+                    </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
-</div>
-{{-- 表单底部操作按钮  --}}
-<div class="bottom">
-    <div class="button-container">
-        <x-layadmin::action :actions="$form['actions']"></x-layadmin::action>
+    {{-- 表单底部操作按钮  --}}
+    <div class="bottom">
+        <div class="button-container">
+            <x-layadmin::action :actions="$form['actions']"></x-layadmin::action>
+        </div>
     </div>
-</div>
 </form>
